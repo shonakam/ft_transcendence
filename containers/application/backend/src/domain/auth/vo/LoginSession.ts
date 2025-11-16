236e35d23aef37faa00480b2b57b13225541d07f
shonakam <g3tsuzumi@gmail.com>
2025-11-17 01:44:46 +0900

    不要なファイルを削除

--- containers/application/backend/src/domain/auth/vo/LoginSession.ts
+++
@@ -1,7 +0,0 @@
-// import { Session } from './TokenService.ts';
-
-// export class LoginSession extends Session {
-//   static create(token: string, ttlMs: number): LoginSession {
-//     return new LoginSession(token, Date.now() + ttlMs);
-//   }
-// }
