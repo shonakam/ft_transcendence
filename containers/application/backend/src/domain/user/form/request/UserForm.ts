67c6bec56771f49d73c041056f0efc69294743f0
shonakam <g3tsuzumi@gmail.com>
2025-11-23 06:31:45 +0900

    リクエストフォームを分離

---
+++ containers/application/backend/src/domain/user/form/request/UserForm.ts
@@ -0,0 +1,24 @@
+export interface CreateUserForm {
+  email: string;
+  username: string;
+  password: string;
+  imagePath: string | null;
+}
+
+export interface UpdateUserForm {
+  username?: string;
+  email?: string;
+  imagePath?: string | null;
+  currentPassword: string;
+  newPassword?: string;
+}
+
+export interface ChangePasswordForm {
+  currentPassword: string;
+  newPassword: string;
+}
+
+export interface DeleteUserForm {
+  email: string;
+  password: string;
+}
