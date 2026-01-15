15c1e0cb020a14fee313c2ba9adca747366a9d9b
Yuki JINI Jinnouchi <yuki.jinnouchi@gmail.com>
2026-01-16 03:12:44 +0900

    mv imputhandler

--- containers/application/frontend/src/components/game/inputHandler/LocalInputHandler.ts
+++ containers/application/frontend/src/components/game/inputHandler/LocalInputHandler.ts
@@ -1,5 +1,5 @@
-import { InputState, InputHandler, PlayerInput } from '@shonakam/common/game/interface/Input';
-import { Direction } from '@shonakam/common/game/types/Direction';
+import { InputState, InputHandler, PlayerInput } from '@shonakam/common/index';
+import type { Direction } from '@shonakam/common/index';
 
 export class LocalInputHandler implements InputHandler {
   keys: Set<string>;
