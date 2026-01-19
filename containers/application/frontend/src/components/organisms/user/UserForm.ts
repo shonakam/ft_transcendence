63d0c4a9665c8ceb014b539c2de827cfcc67fa29
shonakam <g3tsuzumi@gmail.com>
2026-01-19 23:04:00 +0900

    不要なファイルの削除

--- containers/application/frontend/src/components/organisms/user/UserForm.ts
+++
@@ -1,8 +0,0 @@
-// export class UserFormOrganism {
-//   private mode: 'create' | 'update';
-
-//   constructor(mode: 'create' | 'update', initialData?: User) {
-//         this.mode = mode;
-//         // modeに応じて、初期データを入力フィールドに設定するロジック
-//     }
-// }
